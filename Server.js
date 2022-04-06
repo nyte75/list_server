886771d710f04ffad06ee4e939ceff871e7178ee
Nathan Kima Yuave <nate10yuave@protonmail.com>
2022-04-06 14:19:36 +1000

    initial commit

---
+++ Server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express()
+
+app.listen(3000, () =>{
+    console.log('listening at port 3000')
+    console.log('')
+})
+app.use(express.static('public'))
